841dd72facf2e795ed594bdfcff928f54df0d820
Akshay Shekhawat <akshay.shekhawat@gmail.com>
2025-04-19 17:23:58 -0700

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
 import { HyperAgent } from "./agent";
-
-import {
-  HyperbrowserProvider,
-  LocalBrowserProvider,
-} from "./browser-providers";
-
 import { TaskStatus } from "./types/agent/types";
 
-// Export the class directly for both named and default exports
-export { HyperAgent };
+export { TaskStatus, HyperAgent };
 export default HyperAgent;
-
-export { HyperbrowserProvider, LocalBrowserProvider, TaskStatus };
 
 // For CommonJS compatibility
 if (typeof module !== "undefined" && module.exports) {
   module.exports = HyperAgent;
-  module.exports.Hyperbrowser = HyperAgent;
-  module.exports.HyperbrowserAgent = HyperAgent;
+  module.exports.HyperAgent = HyperAgent;
+  module.exports.TaskStatus = TaskStatus;
   module.exports.default = HyperAgent;
 }
